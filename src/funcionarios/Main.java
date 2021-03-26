26160131d2ad8eecacdd7633a1604dec768db758
Alvaro Pereira <alvaropereira@ibm.com>
2021-03-25 22:06:14 -0300

    classes

--- src/funcionarios/Main.java
+++ src/funcionarios/Main.java
@@ -9,8 +9,10 @@ public class Main {
 		
 		p1.setNome("Alvaro");
 		p1.setSobrenome("Pereira");
-		
-		System.out.println(p1.getNome() + " " + p1.getSobrenome());
+		p1.setDataNascimento("11/01/1990");
+		p1.setEstadoCivil(null);
+		p1.setDocIdentidade(null);
+		System.out.println(p1.getNome() + "xxx" + p1.getSobrenome());
 		
 
 	}
